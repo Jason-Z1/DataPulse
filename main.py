2d0577d0ef8a49548b1dbcda699efbbe77a3407b
Jason-Z1 <zhengj12@rpi.edu>
2025-10-24 12:42:35 -0400

    Parse func

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-# Work with 5 min data first, then I can adjust for different use cases
-# Organize the data into a separate file using the Columnar file format (Parquet)
+import os
+
+import pyarrow as pa
+
+# Finds the file directory
+# Is subjected to change in form if we decide to use a different structure layout for data
+def find_file(symbol: str, increm: str) -> str:
+   alpha = str[0]
+   path = "../" + increm + "/FirstData/Downloaded_March15/Historical_stock_full_" + alpha + "_" + increm + "_adj_splitdiv.zip/"
+   path += symbol + "_full_" + increm + "_adjsplitdiv.txt/"
+   return path
+
+
+def parse(filePath: str, startTime: str, endTime: str) -> str:
+   # Should use PyArrow to parse the file given and returns the chunk
+   # of data that is requested.
+
+   chunk = ""
+
+   return chunk
+
+if __name__ == "__main__":
+   path = find_file("AAPL", "1hour")
+
+
+
